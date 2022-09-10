d85bcf638258640647e02fc18e275e360aaece7f
Nkasi-e <nkasijnr@gmail.com>
2022-09-10 03:00:05 +0100

    fixed database connection bug and built route that gets all users data from the database

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,9 +1,17 @@
 const { database } = require("../db/database");
+const pool = require("../db/db");
+const {
+  getAllUsers,
+  registerUsers,
+  getSingleUser,
+} = require("../utils/queries");
 
+//Get all users entries
 const getEntries = async (req, res) => {
-  const user = database.user;
-
-  res.json({ user, nbHit: user.length });
+  await pool.query(getAllUsers, (err, result) => {
+    if (err) throw Error;
+    res.json(result.rows);
+  });
 };
 
 const postEntries = async (req, res) => {
