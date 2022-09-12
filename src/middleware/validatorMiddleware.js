033cc9f2accd19195305342f358d46ade879c4f4
Nkasi-e <nkasijnr@gmail.com>
2022-09-12 14:46:50 +0100

    Created the delete enpoint

--- src/middleware/validatorMiddleware.js
+++ src/middleware/validatorMiddleware.js
@@ -1,7 +1,7 @@
 module.exports = (validator) => {
   return (req, res, next) => {
     const { error } = validator(req.body);
-    console.log("error: ", error);
+    // console.log("error: ", error);
     if (error) {
       return res.status(400).send(error.details[0].message);
     }
--- src/utils/queries.js
+++ src/utils/queries.js
@@ -11,9 +11,13 @@ const existingEmail = "SELECT s FROM users s WHERE s.email = $1";
 const registerEntry =
   "INSERT INTO record (title, description, date) VALUES($1, $2, $3)";
 
+//Deleting Users
+const removeEntry = "DELETE FROM record WHERE id = $1";
+
 module.exports = {
   getAllEntries,
   registerEntry,
   getSingleEntry,
   existingEmail,
+  removeEntry,
 };
