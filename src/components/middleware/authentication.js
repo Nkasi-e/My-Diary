78405bc83cc46385f22cc9a259d067ec7e3f3e10
Nkasi-e <nkasijnr@gmail.com>
2022-10-17 00:07:52 +0100

    Modified the code and Added Authentication Middleware

--- src/components/middleware/authentication.js
+++ src/components/middleware/authentication.js
@@ -2,18 +2,18 @@ const jwt = require('jsonwebtoken');
 
 // AUTHORIZING USERS
 const authMiddleware = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
+  const token = req.headers.authorization;
 
   // Checking if auth header/Bearer token exist
-  if (!authHeader || !authHeader.startsWith('Bearer')) {
+  if (!token || !token.startsWith('Bearer ')) {
     res.status(401).json({ message: `Invalid authentication` });
     throw new Error('Invalid token');
   }
 
-  const token = authHeader.split(' ')[1];
+  const accessToken = token.split(' ')[1];
   try {
     // Verifying token
-    const payload = jwt.verify(token, process.env.JWT_SECRETE);
+    const payload = jwt.verify(accessToken, process.env.JWT_SECRETE);
     const { userId, email } = payload;
     const user = { userId, email };
     req.user = user;
