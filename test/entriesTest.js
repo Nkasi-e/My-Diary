71c6bcadf5a5794fc66f80524096f01764a4332c
Nkasi-e <nkasijnr@gmail.com>
2022-11-21 08:54:09 +0100

    added more test suite for the record enpoints

--- test/entriesTest.js
+++ test/entriesTest.js
@@ -31,38 +31,38 @@ before((done) => {
     });
 });
 
-// describe('POST /api/v1/diary', () => {
-//   it('should create new entry record', (done) => {
-//     chai
-//       .request(server)
-//       .post('/api/v1/diary')
-//       .set({ Authorization: `Bearer ${token}` })
-//       .send(entry)
-//       .end((err, res) => {
-//         if (err) {
-//           throw err;
-//         }
-//         expect(res).to.have.status(201);
-//         done();
-//       });
-//   });
-// });
+describe('GET /api/v1/diary', () => {
+  it('should return no content when entries is empty', (done) => {
+    chai
+      .request(server)
+      .get('/api/v1/diary')
+      .set({ Authorization: `Bearer ${token}` })
+      .end((err, res) => {
+        if (err) {
+          throw err;
+        }
+        expect(res).to.have.status(204);
+        done();
+      });
+  });
+});
 
-// describe('GET /api/v1/diary', () => {
-//   it('should return no content when entries is empty', (done) => {
-//     chai
-//       .request(server)
-//       .get('/api/v1/diary')
-//       .set({ Authorization: `Bearer ${token}` })
-//       .end((err, res) => {
-//         if (err) {
-//           throw err;
-//         }
-//         expect(res).to.have.status(204);
-//         done();
-//       });
-//   });
-// });
+describe('POST /api/v1/diary', () => {
+  it('should create new entry record', (done) => {
+    chai
+      .request(server)
+      .post('/api/v1/diary')
+      .set({ Authorization: `Bearer ${token}` })
+      .send(entry)
+      .end((err, res) => {
+        if (err) {
+          throw err;
+        }
+        expect(res).to.have.status(201);
+        done();
+      });
+  });
+});
 
 describe('GET /api/v1/diary', () => {
   it('should get all user entries', (done) => {
@@ -88,7 +88,7 @@ describe('GET /api/v1/diary/:id', () => {
   it('should get a single entry by id', (done) => {
     chai
       .request(server)
-      .get('/api/v1/diary/17')
+      .get('/api/v1/diary/23')
       .set({ Authorization: `Bearer ${token}` })
       .end((err, res) => {
         if (err) {
@@ -105,7 +105,7 @@ describe('GET /api/v1/diary/:id', () => {
       });
   });
 
-  it('should fail to get bad entry if id doest not exist', (done) => {
+  it('should fail to get diary record if id doest not exist', (done) => {
     chai
       .request(server)
       .get('/api/v1/diary/40')
@@ -114,8 +114,152 @@ describe('GET /api/v1/diary/:id', () => {
         if (err) throw err;
         expect(res).to.have.status(404);
         expect(res.body.error).to.have.property('message');
+        expect(res.body.error.message).to.equal(
+          "Entry with ID NO. 40 doesn't exist"
+        );
         expect(res.body.error).to.be.a('object');
         expect(res.body.error).to.have.property('field').to.equal('id');
+        done();
+      });
+  });
+
+  it('should fail to get diary record because user is unauthorized', (done) => {
+    chai
+      .request(server)
+      .get('/api/v1/diary/15')
+      .set({ Authorization: `Bearer ${token}` })
+      .end((err, res) => {
+        if (err) throw err;
+        expect(res).to.have.status(401);
+        expect(res.body).to.have.property('error').to.be.a('object');
+        expect(res.body.error.message)
+          .to.equal('You are not authorized to view this section')
+          .to.be.a('string');
+        expect(res.body.error.field)
+          .to.equal('authorization')
+          .to.be.a('string');
+        done();
+      });
+  });
+});
+
+describe('PATCH /api/v1/diary/:id', () => {
+  it('should update diary record', (done) => {
+    chai
+      .request(server)
+      .patch('/api/v1/diary/23')
+      .set({ Authorization: `Bearer ${token}` })
+      .send({
+        title: entry.title,
+        body: entry.body,
+      })
+      .end((err, res) => {
+        if (err) {
+          throw err;
+        }
+        expect(res).to.have.status(202);
+        expect(res.body).to.have.property('message').to.be.a('string');
+        expect(res.body.message).to.equal(
+          'Entry with ID number 23 updated successfully'
+        );
+        done();
+      });
+  });
+
+  it('should fail to update diary record', (done) => {
+    chai
+      .request(server)
+      .patch('/api/v1/diary/140')
+      .set({ Authorization: `Bearer ${token}` })
+      .send({
+        title: entry.title,
+        body: entry.body,
+      })
+      .end((err, res) => {
+        if (err) {
+          throw err;
+        }
+        expect(res).to.have.status(404);
+        expect(res.body).to.have.property('error').to.be.a('object');
+        expect(res.body.error.message)
+          .to.equal("Entry with ID NO. 140 doesn't exist")
+          .to.be.a('string');
+        expect(res.body.error.field).to.equal('id').to.be.a('string');
+        done();
+      });
+  });
+
+  it('should fail because the user is unauthorized', (done) => {
+    chai
+      .request(server)
+      .patch('/api/v1/diary/15')
+      .set({ Authorization: `Bearer ${token}` })
+      .send({
+        title: entry.title,
+        body: entry.body,
+      })
+      .end((err, res) => {
+        if (err) {
+          throw err;
+        }
+        expect(res).to.have.status(401);
+        expect(res.body).to.have.property('error').to.be.a('object');
+        expect(res.body.error.message)
+          .to.equal('You are not authorized to update this section')
+          .to.be.a('string');
+        expect(res.body.error.field)
+          .to.equal('authorization')
+          .to.be.a('string');
+        done();
+      });
+  });
+});
+
+describe('DELETE /api/v1/diary/:id', () => {
+  it('should fail when id does not exist', (done) => {
+    chai
+      .request(server)
+      .delete('/api/v1/diary/140')
+      .set({ Authorization: `Bearer ${token}` })
+      .end((err, res) => {
+        if (err) throw err;
+        expect(res).to.have.status(404);
+        expect(res.body).to.have.property('error').to.be.a('object');
+        expect(res.body.error.message)
+          .to.equal("Entry with ID NO. 140 doesn't exist")
+          .to.be.a('string');
+        expect(res.body.error.field).to.equal('id').to.be.a('string');
+        done();
+      });
+  });
+
+  it('should fail when user is unauthorized to perform action', (done) => {
+    chai
+      .request(server)
+      .delete('/api/v1/diary/15')
+      .set({ Authorization: `Bearer ${token}` })
+      .end((err, res) => {
+        if (err) throw err;
+        expect(res).to.have.status(401);
+        expect(res.body).to.have.property('error').to.be.a('object');
+        expect(res.body.error.message)
+          .to.equal('You are not authorized to delete this section')
+          .to.be.a('string');
+        expect(res.body.error.field)
+          .to.equal('authorization')
+          .to.be.a('string');
+        done();
+      });
+  });
+
+  it('should successfully delete record', (done) => {
+    chai
+      .request(server)
+      .delete('/api/v1/diary/23')
+      .set({ Authorization: `Bearer ${token}` })
+      .end((err, res) => {
+        if (err) throw err;
+        expect(res).to.have.status(204);
         done();
       });
   });
