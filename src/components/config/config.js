5b62891c5f9c3ebe348c715fdcf99078bb46d851
Nkasi-e <nkasijnr@gmail.com>
2022-12-22 01:12:34 +0100

    try fixx

--- src/components/config/config.js
+++ src/components/config/config.js
@@ -2,18 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const { DATABASE, DB_USER, DB_PASSWORD } = process.env;
 
-// module.exports = new Sequelize(DATABASE, DB_USER, DB_PASSWORD, {
-//   host: 'localhost',
-//   dialect: 'postgres',
-
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-// });
-const sequelize = new Sequelize(DATABASE, DB_USER, DB_PASSWORD, {
+module.exports = new Sequelize(DATABASE, DB_USER, DB_PASSWORD, {
   host: 'localhost',
   dialect: 'postgres',
 
@@ -24,5 +13,3 @@ const sequelize = new Sequelize(DATABASE, DB_USER, DB_PASSWORD, {
     idle: 10000,
   },
 });
-
-module.exports = { sequelize };
