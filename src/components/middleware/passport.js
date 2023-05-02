f54eadca34401985f1034dde04fafe360134bebb
Nkasi-e <nkasijnr@gmail.com>
2023-05-02 11:24:33 +0100

    Added facebook auth initialization

--- src/components/middleware/passport.js
+++ src/components/middleware/passport.js
@@ -2,12 +2,20 @@ const dotenv = require('dotenv');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const bcrypt = require('bcrypt');
 const { User } = require('../users/model/userModel');
 
 dotenv.config('.env');
 
-const { CLIENT_ID, CLIENT_SECRET, JWT_SECRETE, JWT_LIFETIME } = process.env;
+const {
+  CLIENT_ID,
+  CLIENT_SECRET,
+  JWT_SECRETE,
+  JWT_LIFETIME,
+  FACEBOOK_ID,
+  FACEBOOK_SECRET,
+} = process.env;
 
 passport.serializeUser((user, done) => {
   done(null, user);
@@ -62,11 +70,45 @@ passport.use(
         const token = jwt.sign(payload, JWT_SECRETE, {
           expiresIn: JWT_LIFETIME,
         });
-        // res.redirect(
-        //   `http://localhost:300/auth/social?token=${newUser.createToken(
-        //     newUser.dataValues
-        //   )}&userId=${newUser.id}`
-        // );
+        return done(null, { user: newUser, token });
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: FACEBOOK_ID,
+      clientSecret: FACEBOOK_SECRET,
+      callbackURL: `http://localhost:3000/auth/facebook/callback`,
+      profileFields: ['email', 'displayName'],
+    },
+    async (_req, _refreshToken, _accessToken, profile, done) => {
+      try {
+        const existingUser = await User.findOne({
+          where: { email: profile.emails[0].value },
+        });
+        if (existingUser) {
+          const payload = existingUser.dataValues;
+          const token = jwt.sign(payload, JWT_SECRETE, {
+            expiresIn: JWT_LIFETIME,
+          });
+          return done(null, { user: existingUser, token });
+        }
+        const hashPassword = await bcrypt.hash(profile.id, 10);
+
+        const newUser = await User.create({
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          password: hashPassword,
+        });
+        const payload = newUser.dataValues;
+        const token = jwt.sign(payload, JWT_SECRETE, {
+          expiresIn: JWT_LIFETIME,
+        });
         return done(null, { user: newUser, token });
       } catch (err) {
         return done(err);
